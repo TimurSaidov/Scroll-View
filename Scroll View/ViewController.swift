533ff14999ec3003180f8f20be912c9095fb1806
TimurSaidov <tim77777@inbox.ru>
2018-10-08 18:10:32 +0300

    Initial commit

--- Scroll View/ViewController.swift
+++ Scroll View/ViewController.swift
@@ -10,11 +10,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+//    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        registerForNotification()
+        
+        let tapRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.dismissKeyboard))
+        view.addGestureRecognizer(tapRecognizer)
+        
+//        updateConstraints()
     }
 
+//    func updateConstraints() {
+//        imageView.centerXAnchor.constraint(equalTo: scrollView.contentLayoutGuide.centerXAnchor)
+//        imageView.centerYAnchor.constraint(equalTo: scrollView.contentLayoutGuide.centerYAnchor)
+//    }
+    
+    func registerForNotification() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWasShown(_:)), name: UIResponder.keyboardDidShowNotification, object: nil) // Механизм для события: появления клавиатуры. Когда происходит это событие, вызывается метод: keyboardWasShown(_:)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillBeHidden(_:)), name: UIResponder.keyboardWillHideNotification, object: nil) // Механизм для события: когда клавиатура прячется.
+    }
+    
+    @objc func keyboardWasShown(_ notification: NSNotification) {
+        // Размер клавиатуры.
+        guard let info = notification.userInfo, let keyboardFrameValue = info[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue else { return }
+        
+        let keyboardFrame = keyboardFrameValue.cgRectValue
+        let keyboardSize = keyboardFrame.size
+        
+        // Уменьшение контента Scroll View при появлении клавиатуры.
+        let contentInsets = UIEdgeInsets(top: 0, left: 0, bottom: keyboardSize.height + 20, right: 0)
+        scrollView.contentInset = contentInsets
+        scrollView.scrollIndicatorInsets = contentInsets
+    }
 
+    @objc func keyboardWillBeHidden(_ notification: NSNotification) {
+        let contentInsets = UIEdgeInsets.zero
+        scrollView.contentInset = contentInsets
+        scrollView.scrollIndicatorInsets = contentInsets
+    }
+        
+    @objc private func dismissKeyboard() {
+        view.endEditing(true)
+    }
+    
 }
 
